e2a3baa7b5e57e2985622414598e4e3b203d5a23
Bradley BARBIER <bradley.barbier@outlook.fr>
2024-03-23 11:16:01 +0100

    feat(main): create logic to render marker

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,30 @@ export default class MoreMarkers extends Plugin {
 
 		this.settingsTab = new SettingTab(this.app, this);
 
+		this.registerMarkdownPostProcessor((el, context) => {
+			const markers = [...this.settings.markers];
+			markers.sort((a, b) => b.symbols.length - a.symbols.length);
+
+			let isChanged = false;
+			markers.forEach((marker) => {
+				if (isChanged) {
+					return;
+				}
+
+				const symbols = marker.symbols;
+				const symbolsEscaped = symbols.replace(/[-\\^$*+?.()|[\]{}]/g, '\\$&');
+				const regex = new RegExp(`${symbolsEscaped}(.*)${symbolsEscaped}`, 'g');
+				
+				const beforeChanges = el.innerHTML;
+				el.innerHTML = el.innerHTML.replace(regex, `<span style="background-color: ${marker.color};">$1</span>`);
+				
+				if (beforeChanges !== el.innerHTML) {
+					isChanged = true;
+					console.log(isChanged);
+				}
+			});
+		});
+
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(this.settingsTab);
 	}
