f494e2dc90db6a1bce0f0e2bd0f75cb49a3fd881
Bradley BARBIER <bradley.barbier@outlook.fr>
2024-03-23 12:56:08 +0100

    fix: add highlight if more than one marker is on the same line

--- src/MoreMarkers.ts
+++ src/MoreMarkers.ts
@@ -2,23 +2,12 @@ import { Marker } from './interfaces';
 
 export function searchAndPlaceMarkers(markers: Array<Marker>, el: HTMLElement) {
     const markersSort = [...markers].sort((a, b) => b.symbols.length - a.symbols.length);
-
-    let isChanged = false;
     
     markersSort.forEach((marker) => {
-        if (isChanged) {
-            return;
-        }
         const symbols = marker.symbols;
         const symbolsEscaped = symbols.replace(/[-\\^$*+?.()|[\]{}]/g, '\\$&');
         const regex = new RegExp(`${symbolsEscaped}(.*)${symbolsEscaped}`, 'g');
 
-        const beforeChanges = el.innerHTML;
-        
         el.innerHTML = el.innerHTML.replace(regex, `<span style="background-color: ${marker.color};">$1</span>`);
-
-        if (beforeChanges !== el.innerHTML) {
-            isChanged = true;
-        }
     });
 }
